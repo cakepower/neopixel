123f82799e798d50d55bdd73cd59db95bbb46d26
cakepower <32028127+cakepower@users.noreply.github.com>
2022-11-24 08:15:49 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,37 +1,21 @@
-let strip = neopixel.create(DigitalPin.P11, 24, NeoPixelMode.RGB)
+let strip = neopixel.create(DigitalPin.P13, 24, NeoPixelMode.RGB)
 strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Yellow))
 strip.show()
-huskylens.initI2c()
-huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
 basic.forever(function () {
-    let index: number;
-let index1: number;
-huskylens.request()
-    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+    serial.writeNumber(input.lightLevel())
+    serial.writeLine("")
+    if (input.lightLevel() > 220) {
+        strip.rotate(1)
+        basic.pause(100)
+        strip.show()
+    } else if (input.lightLevel() > 210) {
         strip.showRainbow(1, 360)
         strip.show()
         basic.pause(100)
-        index = 0
-        while (index < 24) {
-            strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Black))
-            strip.rotate(1)
-            strip.show()
-            basic.pause(100)
-            index += 1
-        }
-    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        strip.showRainbow(1, 360)
-        strip.show()
-        basic.pause(100)
-        index1 = 0
-        while (index1 < 24) {
-            strip.setPixelColor(24 - index1, neopixel.colors(NeoPixelColors.Black))
-            strip.rotate(-1)
-            strip.show()
-            basic.pause(100)
-            index1 += 1
-        }
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     } else {
         strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip.clear()
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     }
 })
